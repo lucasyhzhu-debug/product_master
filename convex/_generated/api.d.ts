e4addbabd8489134926b2cd6c6bfa623a7a3c590
lucasyhzhu-debug <lucas.yh.zhu@gmail.com>
2026-06-29 17:41:22 +0700

    chore: regenerate api types after merge

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -287,6 +287,7 @@ import type * as subscriptions__devSeed from "../subscriptions/_devSeed.js";
 import type * as subscriptions_amend from "../subscriptions/amend.js";
 import type * as subscriptions_creditMath from "../subscriptions/creditMath.js";
 import type * as subscriptions_creditOrder from "../subscriptions/creditOrder.js";
+import type * as subscriptions_creditReservation from "../subscriptions/creditReservation.js";
 import type * as subscriptions_delivery from "../subscriptions/delivery.js";
 import type * as subscriptions_enforcement_applyPendingBaselineChanges from "../subscriptions/enforcement/applyPendingBaselineChanges.js";
 import type * as subscriptions_enforcement_cutoffMath from "../subscriptions/enforcement/cutoffMath.js";
@@ -620,6 +621,7 @@ declare const fullApi: ApiFromModules<{
   "subscriptions/amend": typeof subscriptions_amend;
   "subscriptions/creditMath": typeof subscriptions_creditMath;
   "subscriptions/creditOrder": typeof subscriptions_creditOrder;
+  "subscriptions/creditReservation": typeof subscriptions_creditReservation;
   "subscriptions/delivery": typeof subscriptions_delivery;
   "subscriptions/enforcement/applyPendingBaselineChanges": typeof subscriptions_enforcement_applyPendingBaselineChanges;
   "subscriptions/enforcement/cutoffMath": typeof subscriptions_enforcement_cutoffMath;
